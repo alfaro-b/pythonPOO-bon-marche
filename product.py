38eb25d641ab0cfd4f652242a5a44a0d968fae42
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 09:56:30 +0200

    update methods remove_from_stock and add_to_stock in product class

--- product.py
+++ product.py
@@ -2,16 +2,14 @@ class Product:
     name: str = ""
     price: float = 0.0
     stock: float = 0.0
-    unity: float = 0.0
+    unity: str = ""
 
     # ---------------------Methods---------------------
-    def remove_from_stock(self, stock, unity):
-        self.stock = stock - unity
-        return self.stock
+    def remove_from_stock(self, stock, quantity):
+        return stock - quantity
 
-    def add_to_stock(self, stock, unity):
-        self.stock = stock + unity
-        return self.stock
+    def add_to_stock(self, stock, quantity):
+        return stock + quantity
 
     def is_available(self):
         return self.stock > 0
