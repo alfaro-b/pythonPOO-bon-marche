81420b2b0977d376ea7e1f66fe5f314d2854ff01
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-23 13:18:57 +0200

    Ajout de la méthode display_products à la classe Store

--- store.py
+++ store.py
@@ -14,3 +14,15 @@ class Store:
         self.products = products
         self.customers = customers
 
+    def display_products(self) -> None:
+        """
+        Affiche les produits du magasin.
+        """
+        # enumerate permet de parcourir la liste en récupérant l'indice (à partir de 1) et le produit
+        for index, product in enumerate(self.products, start=1):
+            print(
+                # :.2f permet d'afficher le prix avec 2 décimales
+                f"{index}. {product.name} - "
+                f"{product.price:.2f} €/{product.unity} - "
+                f"Stock : {product.stock} {product.unity}"
+            )
