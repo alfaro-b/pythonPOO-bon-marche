c8faeb5687b3da6d7f10a6cc27fc2516ad7044e9
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 13:42:09 +0200

    update comments in product class

--- customer.py
+++ customer.py
@@ -2,15 +2,30 @@ from cart import Cart
 
 
 class Customer:
+
+    """
+    représente un client.
+    """
+
+    # -------------------------Variables----------------------
+
     first_name = ''
     last_name = ''
+
     # --------------------------Methods---------------------
 
+    """
+    Obtenir le nom complet du client.
+    :return : la nom complet du client.
+    """
     def get_full_name(self):
         return self.first_name + ' ' + self.last_name
 
     # ------------------------Constructor-------------------
 
+    """
+    Constructeur avec tous les paramètres.
+    """
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
