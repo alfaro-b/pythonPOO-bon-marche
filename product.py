f01fb4e49c97d411ae04ff3bcca077bc3df69d81
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 09:26:36 +0200

    Add product class with constructor

---
+++ product.py
@@ -0,0 +1,11 @@
+class Product:
+    # ---------------------Methods---------------------
+
+    # -------------------Constructor--------------------
+
+
+def __init__(self, name, price, stock, unity):
+    self.name = name
+    self.price = price
+    self.stock = stock
+    self.unity = unity
