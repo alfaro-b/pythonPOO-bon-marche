865e7c76046004c89b043488f97c4086e40d44d8
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-23 09:41:30 +0200

    ajout de la classe Cart

---
+++ cart.py
@@ -0,0 +1,11 @@
+class Cart:
+    """
+    Représente le panier d'un client.
+    """
+
+    def __init__(self):
+        """
+        Initialise un panier vide.
+        """
+        self.items: list[CartItem] = []
+        # TODO : créer la classe CartItem et l'importer
