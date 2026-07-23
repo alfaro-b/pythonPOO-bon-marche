ea8c62e85533891c0a0a454b1499083f71d11edf
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 11:18:03 +0200

    Add constructor in store class

---
+++ store.py
@@ -0,0 +1,16 @@
+from product import Product
+from customer import Customer
+
+
+class Store:
+
+    # --------------------variables-------------------------
+
+    # ---------------------Methods---------------------------
+
+    # -------------------Constructor-------------------------
+
+    def __init__(self, products, customers):
+        self.products = products
+        self.customers = customers
+
