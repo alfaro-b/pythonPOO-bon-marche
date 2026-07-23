5e66dc4609cfadb79313067b685111dbaf864d66
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 13:30:37 +0200

    Add search_product() method in store class

--- store.py
+++ store.py
@@ -1,3 +1,5 @@
+from enum import nonmember
+
 from product import Product
 from customer import Customer
 
@@ -5,8 +7,17 @@ from customer import Customer
 class Store:
 
     # --------------------variables-------------------------
+    products = []
+    customers = []
 
     # ---------------------Methods---------------------------
+    def search_product(self, product_name):
+        for product in self.products:
+            name_of_product_in_store = product.name.lower()
+            name_of_product_given_by_customer = product_name.lower()
+            if name_of_product_in_store == name_of_product_given_by_customer:
+                return product
+        return None
 
     # -------------------Constructor-------------------------
 
