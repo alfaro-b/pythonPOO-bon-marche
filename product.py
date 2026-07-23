7f12f05f4ca84b61a9d05258269ad1dad34b3871
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-23 09:33:52 +0200

    Add remove_from_stock() method in product class

--- product.py
+++ product.py
@@ -1,5 +1,13 @@
 class Product:
+    name: str = ""
+    price: float = 0.0
+    stock: float = 0.0
+    unity: float = 0.0
+
     # ---------------------Methods---------------------
+    def remove_from_stock(self, stock, unity):
+        self.stock = stock - unity
+        return self.stock
 
     # -------------------Constructor--------------------
 
